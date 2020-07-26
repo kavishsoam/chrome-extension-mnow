2e871994104008afe26b65cdfdd793baab362ec4
kavish <kavishsoam1@gmail.com>
2020-07-26 19:49:02 +0530

    extension push

---
+++ popup.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var checkPageButton = document.getElementById('checkPage');
+    checkPageButton.addEventListener('click', function() {
+        window.open("https://app.massagenow321.com:4433/"); 
+    //   chrome.tabs.getSelected(null, function(tab) {
+    //     d = document;
+  
+    //     // var f = d.createElement('form');
+    //     d.action = 'https://app.massagenow321.com:4433/';
+        // f.method = 'post';
+        // var i = d.createElement('input');
+        // i.type = 'hidden';
+        // i.name = 'url';
+        // i.value = tab.url;
+        // f.appendChild(i);
+        // d.body.appendChild(f);
+        // f.submit();
+      });
+    }, false);
+//   }, false);
